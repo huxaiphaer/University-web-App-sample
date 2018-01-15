bb4624e953df2a0a5da9a6b7058b68c2b3045c23
Huzy_Kamz <huxaiphaeridris@gmail.com>
2018-01-15 22:43:39 +0300

    University sample web App

---
+++ UniversityPortal/Models/Enrollment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityPortal.Models
+{
+    public class Enrollment
+    {
+
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
---
+++ UniversityPortal/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityPortal.Models
+{
+
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }sss
+    }
+}
